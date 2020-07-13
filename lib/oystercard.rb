ce7550feb572a230383852c109bb21a07503f9ec
Student <student@Makerss-Air.broadband>
2020-07-13 16:10:09 +0100

    added in-use status of card and ability to touch in and out

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,10 +1,11 @@
 class OysterCard
-  attr_reader :balance
+  attr_reader :balance, :in_use
 
   CARD_LIMIT = 90
 
   def initialize
     @balance = 0
+    @in_use = false
   end
 
   def top_up(amount)
@@ -17,5 +18,19 @@ class OysterCard
   def deduct(amount)
     @balance -= amount
     "#{amount} deducted"
+  end
+
+  def touch_in
+    @in_use = true
+    "Touch-in successful"
+  end
+
+  def touch_out
+    @in_use = false
+    "Touch-out successful"
+  end
+
+  def in_journey?
+    @in_use
   end
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -22,5 +22,26 @@ describe OysterCard do
       expect(subject.deduct(20)).to eq("20 deducted")
     end
   end
+  describe '#touch_in' do
+    it 'allows a card to register the start of a journey' do
+      expect(subject.touch_in).to eq("Touch-in successful")
+    end
+  end
 
+  describe '#touch_out' do
+    it 'allows a card to register the end of a journey' do
+      expect(subject.touch_out).to eq("Touch-out successful")
+    end
+  end
+
+  describe '#in_journey?' do
+    it 'marks a card as in-use' do
+      subject.touch_in
+      expect(subject.in_journey?).to be true
+    end
+    it 'marks a card as not in-use' do
+      subject.touch_out
+      expect(subject.in_journey?).to be false
+    end
+  end
 end
