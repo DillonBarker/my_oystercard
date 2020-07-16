e3f9cd19cc6b5f0cda7374db99af221735a1ffe9
DillonBarker <barker.dill98@gmail.com>
2020-07-16 16:52:19 +0100

    added functionality of journey log and added the array"

---
+++ lib/journey_log.rb
@@ -0,0 +1,34 @@
+require 'journey'
+
+class JourneyLog
+  attr_reader :this_journey
+  attr_accessor :journey_class
+
+  def initialize(journey_class: Journey)
+    @journey_class = journey_class
+    @journeys = []
+
+  end
+
+  def start(entry_station)
+    @this_journey = journey_class.new(entry_station)
+    @journeys << @this_journey
+
+  end
+
+  def finish(exit_station)
+    this_journey.exit_station = exit_station
+    @journeys << @this_journey
+    @this_journey = nil
+  end
+
+  def journeys
+  @journeys.dup
+  end
+
+  private
+  def this_journey
+    @this_journey ||= journey_class.new
+  end
+
+end
